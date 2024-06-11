a9da5fbc68f08c1476eb0644d1bd49466e8e1585
SOTHYIEN <121343858+SOTHYIEN@users.noreply.github.com>
2024-06-11 19:05:27 +0700

    updateloop

--- console/Loop.js
+++ console/Loop.js
@@ -16,3 +16,14 @@ let Array = [10,20,30];
 // 	i++;
 // }while(i<Array.length);
 
+
+var data;
+// for(i in Array){
+// 	data += console.log(Array[i]);
+// }
+for(key of Array){
+   data+= console.log(key);
+}
+
+
+
