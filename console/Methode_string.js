1cbea614e4ae0dfc2270bed6d970f454399aa08e
SOTHYIEN <121343858+SOTHYIEN@users.noreply.github.com>
2024-06-13 18:50:36 +0700

    add file

---
+++ console/Methode_string.js
@@ -0,0 +1,9 @@
+let str1 = " and james";
+console.log(str1.charAt(2));
+let str2 = "Hello java";
+console.log(str2.indexOf("a"));
+console.log(str2.toLocaleLowerCase());
+console.log(str2.toLocaleUpperCase());
+console.log(str2.slice(0,11));
+var Data =str2.concat(str1);
+console.log(Data);
---
+++ console/Test.js
@@ -0,0 +1,7 @@
+document.write("<h1>Hell javascript</h1>");
+let id,name,sex,school,cshap ,web,ooAD,total;
+id = prompt("Input ID: 🤑");
+name = prompt("Input Name: 👉");
+document.write(`<h2>My Name: ${name}  </h2>`);
+document.write(`<h2>ID is: ${id}</h2>`);
+export{}
